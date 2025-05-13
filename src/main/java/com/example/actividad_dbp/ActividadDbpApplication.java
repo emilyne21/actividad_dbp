acb7ba393956efed188f5bf40168a8acbe4d8329
Amira Perez del Aguila <aepdam21@gmail.com>
2025-05-13 16:56:24 -0500

    Iniciando repo

---
+++ src/main/java/com/example/actividad_dbp/ActividadDbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.actividad_dbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadDbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActividadDbpApplication.class, args);
+    }
+
+}
